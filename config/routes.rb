69b9e60f6edd1c071e9d8e6b37d45e3857966d81
bcarnevale <bianca.carnevale@hotmail.com>
2018-10-19 12:28:32 +1100

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+
+  get 'comments', to: 'posts#index'
+  get 'comments/:id', to: 'posts#index'
+
+  resources :comments
+  root 'posts#index'
+
+  resources :posts
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
